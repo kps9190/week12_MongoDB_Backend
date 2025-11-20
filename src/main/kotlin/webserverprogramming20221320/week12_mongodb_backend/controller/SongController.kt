4b445b4a9affce6f9502ca09296c331d05648f59
ming <k939699@gmail.com>
2025-11-20 18:23:27 +0900

    CORS 오류 해결

--- src/main/kotlin/webserverprogramming20221320/week12_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/webserverprogramming20221320/week12_mongodb_backend/controller/SongController.kt
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 import webserverprogramming20221320.week12_mongodb_backend.model.Song
 import webserverprogramming20221320.week12_mongodb_backend.service.SongService
 
 @RestController
 @RequestMapping("/api/songs") // 제일 뒤에 슬래시 없음에 유의
+//@CrossOrigin(origins = ["http://localhost:5173"]) // CORS 문제 에러
+@CrossOrigin(origins = ["*"]) // CORS 문제 에러
+
 class SongController (
     private val service: SongService
 ) {
